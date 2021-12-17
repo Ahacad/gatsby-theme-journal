9a548bf1049ee75461ab0d975c2dd2d74f0b4625
ahacad <ahacad@126.com>
2021-12-17 20:59:40 +0800

    chore: add gatsby-plugin-root-import

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: "./src/images/icon.png",
       },
     },
     {
@@ -33,5 +34,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-mdx`,
+    "gatsby-plugin-root-import",
   ],
 };
