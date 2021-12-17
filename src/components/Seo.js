bace4d0444417e4f748433f341df00d9b80ccd9f
ahacad <ahacad@126.com>
2021-12-17 22:38:44 +0800

    chore: add new google font

---
+++ src/components/Seo.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+const SEO = () => {
+  return (
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+  );
+};
+
+export default SEO;
