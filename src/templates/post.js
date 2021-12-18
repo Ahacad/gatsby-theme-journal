e446f32ae559c633a407ca747a9679552833170d
ahacad <ahacad@126.com>
2021-12-19 00:20:04 +0800

    feat(post): add basic post component

---
+++ src/templates/post.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import Layout from "src/components/Layout";
+import Leftbar from "src/components/Leftbar";
+import Rightbar from "src/components/Rightbar";
+import "./post.css";
+
+const Post = ({ data, pageContext }) => {
+  const post = data.markdownRemark;
+  console.log("HELLOHELO");
+  console.log(post.html);
+  return (
+    <>
+      <Layout>
+        <Leftbar />
+        <div
+          class="col-span-2 p-8"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        ></div>
+        <Rightbar />
+      </Layout>
+    </>
+  );
+};
+
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "YYYY-MM-DD")
+      }
+    }
+  }
+`;
+
+export default Post;
