8caa4e7090043b4022f46d4003295a03d8344701
ahacad <ahacad@126.com>
2021-12-19 00:09:14 +0800

    chore(seo): seo.js small fix

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -4,7 +4,7 @@ const SEO = () => {
   return (
     <head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
         rel="stylesheet"
