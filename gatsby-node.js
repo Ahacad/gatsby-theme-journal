2ac7b31f226c0102b30ab5bb1814241b47d9c6ad
ahacad <ahacad@126.com>
2021-12-19 00:08:59 +0800

    feat(gatsby-node): add codes for generating each blog page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,42 @@
 const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require("path");
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
+    const value = createFilePath({ node, getNode, basePath: `posts` });
     createNodeField({
       name: `slug`,
       node,
       value,
     });
   }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    const posts = result.data.allMarkdownRemark.edges;
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      });
+    });
+  });
 };
