ad5794034e18f97561a89860b909904be19162fe
ahacad <ahacad@126.com>
2021-12-19 17:13:53 +0800

    chore(post): AP titlify title

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby";
 import Layout from "src/components/Layout";
 import Leftbar from "src/components/Leftbar";
 import Rightbar from "src/components/Rightbar";
+import tit from "ap-style-title-case";
 import "./post.css";
 
 const Post = ({ data, pageContext }) => {
@@ -36,7 +37,7 @@ const Post = ({ data, pageContext }) => {
                 background: `linear-gradient(360deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6) 75%, rgba(255,255,255,0) 100%)`,
               }}
             >
-              <div class="text-3xl">{post.frontmatter.title}</div>
+              <div class="text-3xl">{tit(post.frontmatter.title)}</div>
               <div class="text-xl text-gray-200">
                 {post.frontmatter.description || post.excerpt}
               </div>
